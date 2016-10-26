7a3d114af16ee22ca183772617a7a192b7f994a8
Zeejah Ahmed <Zeejah.A@gmail.com>
2016-10-26 17:51:21 -0400

    This is the initial version of the file that we can start on

---
+++ Motorcycle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MotorcycleBuying{
+
+  public static void main(String[] args){
+
+    String Stripes; // setting variables
+    String Brakes;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("How much does the motorcycle cost?"); //asking for their current cost
+
+    int InitialCost = input.nextInt(); //taking input integer for cost
+
+    System.out.println("Do you want to add stripes on your motorcycle? (yes or no)"); //asking if they want stripes
+
+    Stripes = input.next(); //taking input
+
+    if(Stripes.equals("yes")){ //code to execute if answer is "yes"
+      System.out.println("That will cost 250 dollars more"); 
+
+    }
+
+  }
+}
